92a64a06ca80e043dc19a0ed99c09bfe1b344072
github Takumi-Haito <takumi-haito@kccs.co.jp>
2025-06-05 11:13:14 +0900

    初回コミット

---
+++ ObjBasic0101/src/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		int[] array = {3,8,10,2,4,5};
+		
+		for(int i=0 ; i<array.length ;i++) {
+		System.out.println(array[i]);
+	}
+
+}}
